7698effb2570c2ad74c57dab45960d38593df5a5
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-06-23 09:23:09 +0530

    Create intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,27 @@
+/* What is the output
+
+What will be the output ? */ 
+
+#include <iostream>
+using namespace std;   
+int main()
+{
+    double a = 6 / 4;
+    int b  = 6 / 4;
+    double c = a + b;
+    cout << c;
+}
+
+/* 
+Options
+This problem has only one correct answer
+3
+1.5
+2 
+2.5
+Correct Answer
+
+Solution Description
+When 6 / 4 is performed, both the operands of / are int hence answer will be an int i.e. 1. Hence the value of both a and b is 1. Thus a + b will be 2. In C++, if a double value doesn't contain any decimal value, only integer will be printed.
+*/ 
+
