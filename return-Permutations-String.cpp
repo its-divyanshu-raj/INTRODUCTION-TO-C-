42f592454f31360c164a1c1d56dc03ff78d55def
Divyanshu raj <98219015+its-divyanshu-raj@users.noreply.github.com>
2022-06-27 00:39:08 +0530

    Create return-Permutations-String.cpp

---
+++ return-Permutations-String.cpp
@@ -0,0 +1,42 @@
+/* return Permutations - String
+Send Feedback
+Given a string S, find and return all the possible permutations of the input string.
+Note 1 : The order of permutations is not important.
+Note 2 : If original string contains duplicate characters, permutations will also be duplicates.
+Input Format :
+String S
+Output Format :
+All permutations (in different lines)
+Sample Input :
+abc
+Sample Output :
+abc
+acb
+bac
+bca
+cab
+cba */
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int returnPermutations(string input, string output[]){
+   if(input.size() == 0){
+       output[0] ="";
+   	   return 1;
+   	}
+    
+    string smallString[10000];
+    int smallsize = returnPermutations(input.substr(1), smallString);
+    
+    int idx = 0;
+    for(int i = 0; i < smallsize; i++) {
+        for(int j = 0; j <= smallString[i].size(); j++){
+            output[idx++] = smallString[i].substr(0,j) + input[0] + smallString[i].substr(j);
+        }
+    }
+    
+    return idx;
+       
+}
